997f677c1252b2553ccef9966d8f29d15be5bb38
nancyyanyu <31667959+nancyyanyu@users.noreply.github.com>
2017-09-06 02:06:42 -0400

    Update 566_ReshapetheMatrix.py

--- ArrayProblem/566_ReshapetheMatrix.py
+++ ArrayProblem/566_ReshapetheMatrix.py
@@ -1,1 +1,28 @@
+#566. Reshape the Matrix
+#https://leetcode.com/problems/reshape-the-matrix/description/
 
+'''
+In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different size but keep its original data.
+
+You're given a matrix represented by a two-dimensional array, and two positive integers r and c representing the row number and column number of the wanted reshaped matrix, respectively.
+
+The reshaped matrix need to be filled with all the elements of the original matrix in the same row-traversing order as they were.
+
+If the 'reshape' operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+'''
+
+#my solution
+class Solution(object):
+    def matrixReshape(self, nums, r, c):
+        """
+        :type nums: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        
+
+#smarter solution
+
+
+'''Thought: every other number starting from the second largest number is adopted.'''
