b2a138d509326e767a873057e0654dc473b15a30
nancyyanyu <31667959+nancyyanyu@users.noreply.github.com>
2017-09-06 20:51:35 -0400

    Create 442_FindAllDuplicatesinanArray.py

---
+++ ArrayProblem/442_FindAllDuplicatesinanArray.py
@@ -0,0 +1,17 @@
+#442. Find All Duplicates in an Array
+#https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+
+'''Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements that appear twice in this array.
+
+Could you do it without extra space and in O(n) runtime?'''
+
+#my solution
+
+
+#smarter solution-one line answer 
+
+
+
+'''Thought: '''
