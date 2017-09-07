86844b3856df9f95114c2a1d6d391cf9bd6ccefc
nancyyanyu <31667959+nancyyanyu@users.noreply.github.com>
2017-09-07 00:07:54 -0400

    Create 039_CombinationSum.py

---
+++ ArrayProblem/039_CombinationSum.py
@@ -0,0 +1,15 @@
+#39. Combination Sum
+#https://leetcode.com/problems/combination-sum/description/
+
+'''Given a set of candidate numbers (C) (without duplicates) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.'''
+
+#my solution
+
+
+#smarter solution
+
+
+
+'''Thought: '''
